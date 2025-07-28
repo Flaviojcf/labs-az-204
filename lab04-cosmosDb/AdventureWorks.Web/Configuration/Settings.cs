cd8baa69f0bb8fcdbc6fe32f6fd53ce2170627cb
Flaviojcf <flaviojcostafilho@gmail.com>
2025-07-28 17:40:59 -0300

    feat: Add lab04

---
+++ lab04-cosmosDb/AdventureWorks.Web/Configuration/Settings.cs
@@ -0,0 +1,8 @@
+﻿namespace AdventureWorks.Web.Configuration
+{
+    public class Settings
+    {
+        public string BlobContainerUrl { get; set; }
+        public string BlobSASToken { get; set; }
+    }
+}
